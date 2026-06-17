4af823ae149f48ac00f0dd0f6fa03cb620ece092
ShaharyarShakir <shakirshaharyar125@gmail.com>
2026-06-17 21:32:38 +0500

    Add main server file for OpsMind API with routing and database connection

---
+++ 05__day/opsmind/apps/api/cmd/server/main.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"context"
+	"log"
+	"net/http"
+
+	"github.com/ShaharyarShakir/opsmind-api/internal/database"
+	"github.com/ShaharyarShakir/opsmind-api/internal/documents"
+	"github.com/go-chi/chi/v5"
+)
+
+func main() {
+	db, _ := database.Connect()
+	queries := database.New(db)
+	defer db.Close(context.Background())
+
+	r := chi.NewRouter()
+	docService := documents.Service{
+
+		Queries: queries,
+	}
+
+	r.Get("/", func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte("OpsMind API running"))
+	})
+	r.Mount(
+		"/documents",
+		documents.Routes(docService),
+	)
+	log.Println("server running on :8080")
+
+	log.Fatal(http.ListenAndServe(":8080", r))
+}
