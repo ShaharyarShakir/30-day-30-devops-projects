d2007d70cc52a8d2f1a9c621f6117d74a8784b89
ShaharyarShakir <shakirshaharyar125@gmail.com>
2026-06-17 17:03:53 +0500

    feat: add type exports for HealthResponse and investigation events

---
+++ 04__day/frontend/types/index.ts
@@ -0,0 +1,8 @@
+export type { HealthResponse } from "./health";
+export type {
+  Diagnosis,
+  ProgressEvent,
+  ResultEvent,
+  InvestigationEvent,
+  InvestigationHistoryItem,
+} from "./investigation";
