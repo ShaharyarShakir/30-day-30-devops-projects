aefae16f33aa95213f33fe12ae127a943b788904
ShaharyarShakir <shakirshaharyar125@gmail.com>
2026-06-18 10:10:12 +0500

    chore: update main.go for improved server initialization and routing

--- 05__day/opsmind/apps/api/cmd/server/main.go
+++ 05__day/opsmind/apps/api/cmd/server/main.go
@@ -7,29 +7,64 @@ import (
 
 	"github.com/ShaharyarShakir/opsmind-api/internal/database"
 	"github.com/ShaharyarShakir/opsmind-api/internal/documents"
+	"github.com/ShaharyarShakir/opsmind-api/internal/rag"
+	"github.com/ShaharyarShakir/opsmind-api/internal/search"
+	"github.com/ShaharyarShakir/opsmind-api/internal/storage"
+
 	"github.com/go-chi/chi/v5"
 )
 
 func main() {
-	db, _ := database.Connect()
-	queries := database.New(db)
+
+	db, err := database.Connect()
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
 	defer db.Close(context.Background())
 
-	r := chi.NewRouter()
+	queries := database.New(db)
+
+	// storage service
+	fileStorage := storage.New("./uploads")
+
 	docService := documents.Service{
 
 		Queries: queries,
+
+		Storage: fileStorage,
 	}
+
 	docHandler := documents.NewHandler(docService)
+	searchService := search.Service{
+		Queries: queries,
+	}
+
+	searchHandler := search.NewHandler(searchService)
+	ragService := rag.Service{
+		Queries: queries,
+	}
+
+	ragHandler := rag.NewHandler(ragService)
+
+	r := chi.NewRouter()
 
 	r.Get("/", func(w http.ResponseWriter, r *http.Request) {
+
 		w.Write([]byte("OpsMind API running"))
+
 	})
+
 	r.Mount(
 		"/documents",
 		documents.Routes(docHandler),
 	)
+	r.Mount("/search", search.Routes(searchHandler))
 	log.Println("server running on :8080")
+	r.Mount("/ask", rag.Routes(ragHandler))
+	log.Fatal(
+		http.ListenAndServe(":8080", r),
+	)
 
-	log.Fatal(http.ListenAndServe(":8080", r))
 }
