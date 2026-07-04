18128d73f7d175ecfe202cf060ce60b500e959d2
ShaharyarShakir <shakirshaharyar125@gmail.com>
2026-07-04 14:42:31 +0500

    refactor: handle file closure with error suppression in LoadVaultSecrets

--- 06__day/backend/internal/config/vault.go
+++ 06__day/backend/internal/config/vault.go
@@ -21,7 +21,7 @@ func LoadVaultSecrets() error {
 	if err != nil {
 		return err
 	}
-	defer file.Close()
+	defer func() { _ = file.Close() }()
 
 	scanner := bufio.NewScanner(file)
 	for scanner.Scan() {
