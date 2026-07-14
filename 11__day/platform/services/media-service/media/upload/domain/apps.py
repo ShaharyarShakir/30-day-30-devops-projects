6a3d2ed00e7034d11d31a25f5821357fab4b0769
ShaharyarShakir <shakirshaharyar125@gmail.com>
2026-07-14 10:28:57 +0500

    feat: add UploadDomainConfig for media upload domain management

---
+++ 11__day/platform/services/media-service/media/upload/domain/apps.py
@@ -0,0 +1,7 @@
+from django.apps import AppConfig
+
+
+class UploadDomainConfig(AppConfig):
+    default_auto_field = "django.db.models.BigAutoField"
+    name = "media.upload.domain"
+    verbose_name = "Upload Domain"
