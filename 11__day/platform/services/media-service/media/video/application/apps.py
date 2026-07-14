0807daf006af3e93bd34ef0badeac992088b7f55
ShaharyarShakir <shakirshaharyar125@gmail.com>
2026-07-14 10:33:08 +0500

    feat: add VideoApplicationConfig for video application management

---
+++ 11__day/platform/services/media-service/media/video/application/apps.py
@@ -0,0 +1,7 @@
+from django.apps import AppConfig
+
+
+class VideoApplicationConfig(AppConfig):
+    default_auto_field = "django.db.models.BigAutoField"
+    name = "media.video.application"
+    label = "video_application"
