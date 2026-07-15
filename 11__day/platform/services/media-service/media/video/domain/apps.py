c6d55a7d2ab840512953f088fc340ad66156e0ac
ShaharyarShakir <shakirshaharyar125@gmail.com>
2026-07-15 14:32:41 +0500

    feat: add VideoDomainConfig for video domain management

---
+++ 11__day/platform/services/media-service/media/video/domain/apps.py
@@ -0,0 +1,7 @@
+from django.apps import AppConfig
+
+
+class VideoDomainConfig(AppConfig):
+    default_auto_field = "django.db.models.BigAutoField"
+    name = "media.video.domain"
+    label = "video_domain"
